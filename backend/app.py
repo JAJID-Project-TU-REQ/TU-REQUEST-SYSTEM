bc189e19bb36da16ff51f43db15a63a35e5be5f8
jayminrp <rapeephanwadee.plo@dome.tu.ac.th>
2024-11-25 01:55:42 +0700

    add frontend vercel url

--- backend/app.py
+++ backend/app.py
@@ -19,7 +19,7 @@ origins = ["http://localhost:3000"] # Replace with your frontend URL
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Allows specified origins
+    allow_origins=["https://tu-request-system-theta.vercel.app/"],  # Allows specified origins
     allow_credentials=True,
     allow_methods=["*"],  # Allows all HTTP methods
     allow_headers=["*"],  # Allows all headers
